9e896e14ff888b32a01d91d6062a6d06a97b1fb6
Alex <alex@sitepoint.com>
2017-11-27 09:53:16 +1100

    First commit

---
+++ src/docs/markade.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "name": "Front-end Performance Site",
+  "directories": {
+    "output": "html",
+    "template": "templates",
+    "data": "md"
+  },
+  "templates": {
+    "index.pug": [
+      "index.md"
+    ],
+    "front-matter.pug": [
+      "front-matter.md"
+    ]
+  }
+}
